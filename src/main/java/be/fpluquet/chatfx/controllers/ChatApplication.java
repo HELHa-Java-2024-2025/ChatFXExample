eec4ee07ae3ef5282966a5d195b9ce495601bb7a
fpluquet <fredericpluquet@gmail.com>
2025-04-09 09:06:10 +0200

    implement chat message functionality

--- src/main/java/be/fpluquet/chatfx/controllers/ChatApplication.java
+++ src/main/java/be/fpluquet/chatfx/controllers/ChatApplication.java
@@ -8,7 +8,9 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class ChatApplication extends Application {
+public class ChatApplication extends Application implements ConversationViewController.Listener {
+    private ConversationViewController conversationViewController;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ConversationViewController.class.getResource("conversation-view.fxml"));
@@ -16,9 +18,17 @@ public class ChatApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        
+        conversationViewController = fxmlLoader.getController();
+        conversationViewController.setListener(this);
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void askToAddMessage(String message) {
+        conversationViewController.addMessageToChat(message);
     }
 }
--- src/main/java/be/fpluquet/chatfx/views/ConversationViewController.java
+++ src/main/java/be/fpluquet/chatfx/views/ConversationViewController.java
@@ -1,14 +1,58 @@
 package be.fpluquet.chatfx.views;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class ConversationViewController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField messageTextField;
+
+    @FXML
+    private VBox messagesVBox;
+
+    @FXML
+    private Button sendButton;
+
+    private Listener listener;
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
     }
+
+    @FXML
+    void initialize() {
+        sendButton.setOnAction(event -> {
+            askToAddMessage();
+        });
+
+        messageTextField.setOnKeyTyped(keyEvent -> {
+            // TODO : Check with keyEvent.getCode() == KeyCode.ENTER
+            if (keyEvent.getCharacter().equals("\r")) {
+                askToAddMessage();
+            }
+        });
+    }
+
+    private void askToAddMessage() {
+        String message = messageTextField.getText();
+        this.listener.askToAddMessage(message);
+    }
+
+    public void addMessageToChat(String message) {
+        if (!message.isEmpty()) {
+            Label messageLabel = new Label("Fréd : " + message);
+            messagesVBox.getChildren().add(messageLabel);
+            messageTextField.clear();
+            messageTextField.requestFocus();
+        }
+    }
+
+    public interface Listener {
+        void askToAddMessage(String message);
+    }
+
 }
